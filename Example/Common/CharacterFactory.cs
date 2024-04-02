5cfbd90cd3c634a97c3eb522c6329e888d3278e4
k0fe <x.k0fe@yandex.ru>
2024-04-02 03:15:48 -0300

    fix errors

--- Example/Common/CharacterFactory.cs
+++ Example/Common/CharacterFactory.cs
@@ -12,10 +12,11 @@ public class CharacterFactory
         var x = GameSettings.MAP_WIDTH / 2;
         var y = GameSettings.MAP_HEIGHT / 2;
         var entity = CreateCharacter(GameSettings.PLAYER_SYMBOL, x, y, ConsoleColor.Yellow);
-        entity.SetComponent(new PlayerComponent
+        var playerComponent = new PlayerComponent
         {
             Level = 1,
-        });
+        };
+        entity.SetComponent(ref playerComponent);
         return entity;
     }
 
@@ -24,11 +25,12 @@ public class CharacterFactory
         var x = random.Next(0, GameSettings.MAP_WIDTH);
         var y = random.Next(0, GameSettings.MAP_HEIGHT);
         var entity = CreateCharacter(GameSettings.NPC_SYMBOL, x, y, ConsoleColor.Blue);
-        entity.SetComponent(new DecisionDelayComponent
+        var decisionDelay = new DecisionDelayComponent
         {
             Delay = GameSettings.NPC_DECISION_DELAY,
             CurrentDelay = 0,
-        });
+        };
+        entity.SetComponent(ref decisionDelay);
         return entity;
     }
 
@@ -36,17 +38,20 @@ public class CharacterFactory
     {
         var world = World.Default();
         var entity = world.CreateEntity();
-        entity.SetComponent(new PositionComponent
+        var positionComponent = new PositionComponent
         {
             X = x,
             Y = y
-        });
-        entity.SetComponent(new CharacterComponent());
-        entity.SetComponent(new SymbolComponent
+        };
+        entity.SetComponent(ref positionComponent);
+        var characterComponent = new CharacterComponent();
+        entity.SetComponent(ref characterComponent);
+        var symbolComponent = new SymbolComponent
         {
             Value = symbol,
             Color = color,
-        });
+        };
+        entity.SetComponent(ref symbolComponent);
 
         return entity;
     }
